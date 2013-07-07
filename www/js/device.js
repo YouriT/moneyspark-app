ea5553f2b6b766c094c443d0a2257e5f95501a27
Cyril LEQUEUX <l.cyril@gmail.com>
2013-07-08 00:02:20 +0200

    scroll

--- www/js/device.js
+++ www/js/device.js
@@ -1,8 +1,27 @@
+var resize = function(){
+	windowHeight = window.screen.height;
+	bodyHeight = $(".ui-page-active").outerHeight();
+	diff = (bodyHeight-windowHeight);
+	if(diff > 0){
+		ineH = $(".ana .text").height();
+		$(".ana .text").height(ineH-diff);
+
+		
+		if(device.platform == "Android" && parseInt(device.version) < 3){
+			$(".ana .text").niceScroll();
+			console.log(device.platform+" niceScroll");
+		}
+		else
+		{
+			$(".ana .text").css("overflow-y", "scroll");
+		}
+	}
+}
+
 $(function(){
-    function onDeviceReady() {
+	function onDeviceReady() {
+		resize();
         navigator.splashscreen.hide();
-        // console.log(parseInt( device.version, 10 ));
-        //parseInt( device.version, 10 ) < 4
-    }
-	document.addEventListener("deviceready", onDeviceReady, false);
+     }
+     document.addEventListener("deviceready", onDeviceReady, false);
 });
--- www/js/project.js
+++ www/js/project.js
@@ -2,8 +2,10 @@ $('.imin').click(function(){
 	$('.buydeal').toggleClass('active');
 	if (!$('.buydeal').hasClass('active')) {
 		setTimeout(function () {$('.buydeal').css({marginTop: 0});}, 300);
+		$("body").css("overflow-y", "hidden");
 	}
 	else {
 		$('.buydeal').css({marginTop: -10/16+'em'});
+		$("body").css("overflow-y", "hidden");
 	}
 });
