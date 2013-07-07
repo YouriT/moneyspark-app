85c373c1f7bdf0635c5fabf7a0ccbb09359182f4
Youri Tolstoy <ytolstoy@gmail.com>
2013-07-07 23:02:38 +0200

    Debug

--- www/js/device.js
+++ www/js/device.js
@@ -1,8 +1,8 @@
 $(function(){
-document.addEventListener("deviceready", onDeviceReady, false);
-            function onDeviceReady() {
-                navigator.splashscreen.hide();
-                console.log(parseInt( device.version, 10 ));
-                //parseInt( device.version, 10 ) < 4
-            }
+    function onDeviceReady() {
+        navigator.splashscreen.hide();
+        // console.log(parseInt( device.version, 10 ));
+        //parseInt( device.version, 10 ) < 4
+    }
+	document.addEventListener("deviceready", onDeviceReady, false);
 });
