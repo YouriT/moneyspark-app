55c6e82d167e3f42af9c719c506fdc0dc2d47318
Youri Tolstoy <ytolstoy@gmail.com>
2013-07-07 21:55:17 +0200

    Buy deal animation

--- www/js/project.js
+++ www/js/project.js
@@ -1,9 +1,9 @@
 $('.imin').click(function(){
-
-
-$('.buydeal').animate({
-    height: 'toggle'
-  }, 500, function() {
-  });
-
+	$('.buydeal').toggleClass('active');
+	if (!$('.buydeal').hasClass('active')) {
+		setTimeout(function () {$('.buydeal').css({marginTop: 0});}, 300);
+	}
+	else {
+		$('.buydeal').css({marginTop: -10/16+'em'});
+	}
 });
