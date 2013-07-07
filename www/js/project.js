0f7787442de1b30a6e397d32ed091e36a8bc33b6
Cyril LEQUEUX <l.cyril@gmail.com>
2013-07-07 19:36:19 +0200

    invest

--- www/js/project.js
+++ www/js/project.js
@@ -1,14 +1,9 @@
 $('.imin').click(function(){
 
-	$('.deal').addClass("a");
-	$('.bordertop').before('</div>');
-	$('.borderbottom').after('<div class="row-fluid deal b">');
 
-	$('.buydeal').show();
-
-	$('.bordertop, .borderbottom').hide();
-
-
-
+$('.buydeal').animate({
+    height: 'toggle'
+  }, 500, function() {
+  });
 
 });
