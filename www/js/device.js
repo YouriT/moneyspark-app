2a4daec75c42c09e08981ca9f7d9d69b12c02003
Youri Tolstoy <ytolstoy@gmail.com>
2013-07-08 12:14:56 +0200

    Hauteur adaptative sur index

--- www/js/device.js
+++ www/js/device.js
@@ -1,31 +1,33 @@
-var resize = function(){
-	var windowHeight = window.screen.height;
-	var bodyHeight = $(".ui-page-active").outerHeight();
-	var diff = $(".ana .text").height() - bodyHeight - windowHeight;
-	if(diff > 0){
-		
-		$(".ana .text").height(diff);
-
-		if(device.platform == "Android" && parseInt(device.version) < 3){
-			$(".ana .text").niceScroll();
-		}
-		else
-		{
-			$(".ana .text").css("overflow-y", "scroll");
-		}
-	}
-	else
-	{
-		// cas où il faut mettre les puces en bas...
-	}
-}
-
 $(function(){
+	/*global device*/
+
+	var resize = function(){
+		var windowHeight = window.screen.height;
+		var bodyHeight = $(".ui-page-active").outerHeight();
+		var diff = $(".ana .text").height() - bodyHeight - windowHeight;
+		if(diff > 0) {
+			
+			$(".ana .text").height(diff);
+
+			if(device.platform === "Android" && parseInt(device.version, 10) < 3){
+				$(".ana .text").niceScroll();
+			}
+			else {
+				$(".ana .text").css("overflow-y", "scroll");
+			}
+		}
+		else {
+			var heightDiff = $(window).height() - $('.ensemble-pagenum').position().top - $('.ensemble-pagenum').height() - parseInt($('.ensemble-pagenum').css('marginTop'), 10) - parseInt($('.ensemble-pagenum').css('marginBottom'), 10);
+			$(".ana .text").height($(".ana .text").height()+heightDiff);
+		}
+	};
+
 	function onDeviceReady() {
 		navigator.splashscreen.hide();
-    }
-    document.addEventListener("deviceready", onDeviceReady, false);
-    $(window).load(function () {
-    	resize();
-    });
+	}
+	
+	document.addEventListener("deviceready", onDeviceReady, false);
+	$(window).load(function () {
+		resize();
+	});
 });
