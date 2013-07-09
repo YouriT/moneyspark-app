3ac0b742f723d78ecba6239b3cd38d24cbe196d9
Youri Tolstoy <ytolstoy@gmail.com>
2013-07-09 15:28:47 +0200

    Flip effect

--- www/js/device.js
+++ www/js/device.js
@@ -1,24 +1,19 @@
 $(function(){
 	/*global device*/
-	var resize = function(){
-		var anaHeight = $(window).height() - $('.ana > .text').offset().top - ($(window).height()*0.04+118+(0.62+0.75*16))/16*parseInt($('body').css('font-size'),10);
-		$(".ana .text").height(anaHeight);
-
-		
-		// if(device && device.platform === "Android" && parseInt(device.version, 10) < 3) {
-			$(".ana .text").niceScroll();
-		// }
-		// else {
-		// 	$(".ana .text").css({'overflow-y': 'scroll'});
-		// }
+	var resizeProduct = function (obj) {
+		var anaHeight = $(window).height() - $('.ana > .text', obj.target).offset().top - ($(window).height()*0.04+118+(0.62+0.75*16))/16*parseInt($('body').css('font-size'),10);
+		$(".ana .text", obj.target).height(anaHeight);
+		$(".ana .text", obj.target).niceScroll();
 	};
 
 	function onDeviceReady() {
-		navigator.splashscreen.hide();
+		if (device.platform.name === '') {
+			navigator.splashscreen.hide();
+		}
 	}
 	
 	document.addEventListener("deviceready", onDeviceReady, false);
 	$(window).load(function () {
-		resize();
+		$(window).bind('resize-product', resizeProduct);
 	});
 });
--- www/js/project.js
+++ www/js/project.js
@@ -1,11 +1,51 @@
-$('.imin').click(function(){
-	$('.buydeal').toggleClass('active');
-		if (!$('.buydeal').hasClass('active')) {
-			setTimeout(function () {$('.buydeal').css({marginTop: 0});}, 300);
-		$("body").css("overflow-y", "hidden");
-	}
-	else {
-		$('.buydeal').css({marginTop: -10/16+'em'});
-		$("body").css("overflow-y", "hidden");
-	}
+function iminClick () {
+		$('button.letsgo').click(function () {
+			$('.flip-container').toggleClass('hover');
+			console.log('test');
+		});
+		$('.imin').click(function(){
+		$('.buydeal').toggleClass('active');
+			if (!$('.buydeal').hasClass('active')) {
+				setTimeout(function () {$('.buydeal').css({marginTop: 0});}, 300);
+			$("body").css("overflow-y", "hidden");
+		}
+		else {
+			$('.buydeal').css({marginTop: -10/16+'em'});
+			$("body").css("overflow-y", "hidden");
+		}
+	});
+}
+(function ($) {
+	$.fn.changePage = function (page) {
+		$.ajaxSetup ({
+			cache: false
+		});
+		$.get(page, function (r) {
+			console.log($(r).html());
+			// // $('#page').css({position:'absolute',top:0,left:0,backfaceVisibility: 'hidden'});
+			// $('body').apped($(r).html());
+			// $('.deal').transition({
+			// 	perspective: '1200px',
+			// 	rotateY: '180deg'
+			// }, function () {
+			// 	// $('body').prop('class',$(r).find('#page').prop('class'));
+
+			// 	$('.deal').transition({
+			// 		perspective: '1200px',
+			// 		rotateY: '180deg'
+			// 	});
+			// });
+		});
+		// $('body').load(page+ '#page', function () {
+		// 	$('body').prop('class',$('#page').prop('class'));
+		// 	$('body').transition({
+		// 		perspective: '100px',
+		// 		rotateY: '180deg'
+		// 	});
+		// });
+	};
+}(jQuery));
+
+$(window).load(function () {
+	$(window).bind('imin', iminClick);
 });
